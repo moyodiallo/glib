e91fa4716a806986cb06dda99f90cc39368de517
moyo <moyo3ib@gmail.com>
2020-02-07 18:50:36 +0100

    n_of_graph: finding max id and edge size

--- glib.h
+++ glib.h
@@ -2,13 +2,18 @@
 #define _GLIB_H_
 
 typedef struct {
+	unsigned long x;
+	unsigned long y;
+} couple;
+
+typedef struct {
 	unsigned long s;
 	unsigned long t;
 } edge;
 
 /*edge list structure:*/
 typedef struct edgelist{
-	unsigned long n;        /*number of nodes*/
+	unsigned long n;        /*number of nodes (properly the maximum id)*/
 	unsigned long e;        /*number of edges*/
 	edge *edges;            /*list of edges*/
     char directed;        /*1 if directed else 1*/
@@ -16,18 +21,21 @@ typedef struct edgelist{
 
 /*edge list structure:*/
 typedef struct adjlist{
-    unsigned long n;        /*number of nodes*/
+    unsigned long n;        /*number of nodes (properly the maximum id)*/
 	unsigned long e;        /*number of edges*/
-    char undirected;        /*1 if directed else 0*/
+    char directed;        /*1 if directed else 0*/
     unsigned long *cd;      /*cumulative degree cd[0]=0 length=n+1*/
 	unsigned long *adj;     /*concatenated lists of neighbors of all nodes*/
 } adjlist;
 
-/*edge list structure:*/
+/*
+	edge list structure:
+	This structure is not suitable to manipulate a graph
+*/
 typedef struct adjmatrix{
-    unsigned long n;        /*number of nodes*/
+    unsigned long n;        /*number of nodes (properly the maximum id)*/
 	unsigned long e;        /*number of edges*/
-    char undirected;        /*1 if directed else 0*/
+    char directed;        /*1 if directed else 0*/
 	char* mat;              /*adjacency matrix*/
 } adjmatrix;
 
@@ -38,9 +46,44 @@ edgelist*  make_edgelist_file(char* input, int n_of_nodes, int n_of_edges, char 
 adjlist*   make_adjlist_edges(edgelist*);
 adjmatrix* make_adjmatrix_edges(edgelist*);
 
+/*
+	build from edges list by sorting neighbors nodes
+*/
+adjlist*   make_adjlist_edges_sort(edgelist*);
 
+/*
+	generate a graph with 4 cluster
+	each pair of node in same cluster is connected with probability p
+	each pair of node in different cluster is connected with probability q
+	the graph is printed
+	the probability is between [0-1]
+*/
+void generate4clusters(unsigned long n_of_nodes, double p, double q);
+
+
+/*
+	free the structures
+*/
 void free_edgelist(edgelist*);
 void free_adjlist(adjlist*);
 void free_adjmatrix(adjmatrix*);
+
+/*
+	couple 
+		x -> n_of_nodes
+		y -> n_of_edges
+*/
+couple n_of_graph(char*);
+
+/*
+	
+*/
+
+/*
+	printing for testing little graphs
+*/
+void print_adjlist(adjlist*);
+void print_edgelsit(edgelist*);
+void print_n_of_graph(char*);
 
 #endif
