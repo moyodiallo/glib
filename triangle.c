a03c81c1ca856b301a8bd55a0bafd353719f90e8
moyo <moyo3ib@gmail.com>
2020-02-10 19:48:34 +0100

    triangle.c: compute triangles of graph

---
+++ triangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "glib.h"
+
+int main(int argc, char** argv){
+    if (argc != 5){
+        printf("fail: cmd <file> <n_of_node> <n_of_edges> <p or n>");
+        exit(EXIT_FAILURE);
+    }
+
+    char* file  = argv[1]; 
+    unsigned long n_of_node = atoi(argv[2]);
+    unsigned long n_of_edge = atoi(argv[3]);
+
+    /*the edge list have to be directed*/
+    edgelist* e_list = make_edgelist_file(file,n_of_node,n_of_edge,1);
+    adjlist*  a_list = make_adjlist_edges(e_list); 
+
+    if(argv[4][0] == 'p')
+        compute_triangle(a_list,1);
+    else if(argv[4][0] == 'n')
+        compute_triangle(a_list,0);
+    else
+    {
+        printf("fail: cmd <file> <n_of_node> <n_of_edges> <p or n>");
+        exit(EXIT_FAILURE);
+    }    
+}
